42ec0aabf2edab179cc7561c8863622e561b42b9
sethdaking <lastemail768@gmail.com>
2025-07-08 06:09:55 +0200

    Implemented classes and instances

--- debug.c
+++ debug.c
@@ -58,6 +58,10 @@ int disassembleInstruction(Chunk* chunk, int offset) {
             return simpleInstruction("OP_NIL", offset);
         case OP_TRUE:
             return simpleInstruction("OP_TRUE", offset);
+        case OP_GET_PROPERTY:
+            return constantInstruction("OP_GET_PROPERTY", chunk, offset);
+        case OP_SET_PROPERTY:
+            return constantInstruction("OP_SET_PROPERTY", chunk, offset);
         case OP_EQUAL:
             return simpleInstruction("OP_EQUAL", offset);
         case OP_GREATER:
@@ -126,6 +130,8 @@ int disassembleInstruction(Chunk* chunk, int offset) {
 
             return offset;
         }
+        case OP_CLASS:
+            return constantInstruction("OP_CLASS", chunk, offset);
         default:
             printf("Unknown opcode %d\n", instruction);
             return offset + 1;
--- memory.c
+++ memory.c
@@ -73,13 +73,18 @@ static void markArray(ValueArray* array) {
 }
 
 static void blackenObject(Obj* object) {
-#ifdef DEBU_LOG_GC
+#ifdef DEBUG_LOG_GC
     printf("%p blacken ", (void*)object);
     printValue(OBJ_VAL(object));
     printf("\n");
 #endif
 
     switch (object->type){
+        case OBJ_CLASS: {
+            ObjClass* klass = (ObjClass*)object;
+            markObject((Obj*)klass->name);
+            break;
+        }
         case OBJ_CLOSURE: {
             ObjClosure* closure = (ObjClosure*)object;
             markObject((Obj*)closure->function);
@@ -93,6 +98,12 @@ static void blackenObject(Obj* object) {
             markObject((Obj*)function->name);
             markArray(&function->chunk.constants);
             break;
+        case OBJ_INSTANCE: {
+            ObjInstance* instance = (ObjInstance*)object;
+            markObject((Obj*)instance->klass);
+            markTable(&instance->fields);
+            break;
+        }
         case OBJ_UPVALUE:
             markValue(((ObjUpvalue*)object)->closed);
             break;
@@ -107,6 +118,10 @@ static void freeObject(Obj* object) {
     printf("%p free type %d\n", (void*)object, object->type);
 #endif
     switch (object->type) {
+        case OBJ_CLASS: {
+            FREE(ObjClass, object);
+            break;
+        }
         case OBJ_CLOSURE: {
             ObjClosure* closure = (ObjClosure*)object;
             FREE_ARRAY(ObjUpvalue*, closure->upvalues, closure->upvalueCount);
@@ -117,6 +132,12 @@ static void freeObject(Obj* object) {
             ObjFunction* function = (ObjFunction*)object;
             freeChunk(&function->chunk);
             FREE(ObjFunction, object);
+            break;
+        }
+        case OBJ_INSTANCE: {
+            ObjInstance* instance = (ObjInstance*)object;
+            freeTable(&instance->fields);
+            FREE(ObjInstance, object);
             break;
         }
         case OBJ_NATIVE: {
